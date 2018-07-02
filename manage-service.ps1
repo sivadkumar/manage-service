fb22509e2a01b91c53acc05af0e2b4efd2fab6c5
U-CORP\sd2 <sd2@Intuita350269.corp.intuit.net>
2018-07-01 20:28:25 -0700

    Initial Commit

---
+++ manage-service.ps1
@@ -0,0 +1,33 @@
+﻿
+Param(
+[string]$servicename,
+[string]$serviceaction
+)
+write-host "Name of the Service you want to Manage: $servicename"
+write-host "Action you want to Perform on the Service: $serviceaction"
+
+
+function validateConnection {
+
+    param($compName)
+
+    if ( (Test-Connection -ComputerName $compName -Count 2 -Quiet) -eq "True" )
+    {
+        Write-Host "$compName is Online" -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "$compName is Offline" -ForegroundColor Red
+    }
+
+}
+
+
+
+$hostnames = @("192.168.145.140", "192.168.145.145")
+
+
+
+ForEach ($hostname in $hostnames) {
+    validateConnection -compName $hostname
+}
